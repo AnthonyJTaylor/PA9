6204c5827ea16101c27aba97b05e516324a986ac
AnthonyJTaylor <33767381+AnthonyJTaylor@users.noreply.github.com>
2017-12-05 13:21:31 -0800

    Create network_story.cpp

---
+++ network_story.cpp
@@ -0,0 +1,18 @@
+sf::Text story("A man enters your booth raving about the merrits of long action large calliber rifels for war as compared to the currrently accepted docterine using smaller callibers and supression fire. Believing him to be a high ordinator of the holy church of Kube you feel you should accept him into the city");
+sf::Text pass("You allow the man into the city, he thanks you and hands you a spam can filled with ammunation for your duty rifle. As he walks through the barriers to the city you hear him  ranting about sptizer ammunition being the devils pills. You feel the glowing merrits of hardball ammunition in your heart. +1 life");
+sf::Text fail("The man declares "I am a cardinal of the Kube, Feel the might of 7.62 x 63" After one of the mans bodyguards shoots you, you feel deep pain. Not just from the gaping flesh wound but also deep shame from failing your trainging and intution. The man and his enturage then walks through your baricade unmolested. -2 life");
+
+      story.draw();
+			while(window.pollEvent(event) != sf::Event::Closed)
+			{
+				if(keyboard.isKeyPressed((sf::Keyboard::Z)){
+					fail.draw();
+          data.decrementlives();
+          data.decrementlives();
+					break;
+				}
+				else if(keyboard.isKeyPressed((sf::Keyboard::X){
+					pass.draw();
+					break;	
+				}
+      }
