d42b7e4108ad322f8aad81693e2d9b5f754a23ba
AnthonyJTaylor <33767381+AnthonyJTaylor@users.noreply.github.com>
2017-12-05 15:33:29 -0800

    Add files via upload

---
+++ SFML_Test3/SFML_Test3/SFML_Test3/Server.cpp
@@ -0,0 +1,33 @@
+#include <SFML/Graphics.hpp>
+#include<iostream>
+#include <SFML/Network.hpp>
+const int port = 303;
+std::string ip("192.168.0.100");
+sf::TcpSocket socket;
+const std::string msg("go");
+
+void server(void)
+{
+	sf::TcpListener listner;
+	listner.setBlocking(true);
+	listner.listen(port);
+	listner.accept(socket);
+}
+void pusher(void)
+{
+	server();
+	sf::Packet packet_o;
+	while (1)
+	{
+		std::cout << "Press Enter to send package";
+		std::cin.get(); //get user input, dont do anything
+		packet_o << msg; // push string into packet
+		socket.send(packet_o);
+	}
+}
+int main(void)
+{
+	std::cout << "Input output ip: ";
+	std::cin >> ip;
+	pusher();
+}
